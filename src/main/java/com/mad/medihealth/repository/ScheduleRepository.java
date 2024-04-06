d728dd19db18214eb3bc6900a627f73b0bfb54a9
SonNgo <chronos12022002@gmail.com>
2024-04-06 21:10:48 +0700

    Edit task

--- src/main/java/com/mad/medihealth/repository/ScheduleRepository.java
+++ src/main/java/com/mad/medihealth/repository/ScheduleRepository.java
@@ -27,7 +27,21 @@ public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
             + "ORDER BY schedules.time;\r\n", nativeQuery = true)
     List<Schedule> getListScheduleofToday(@Param("userId") String userId);
     
-    Iterable<Schedule> findAllByIsActiveIsTrueAndPrescriptionDrugUserIsActiveIsTrue();
+    @Query(value = "SELECT schedules.*\r\n"
+            + "FROM schedules\r\n"
+            + "JOIN prescriptions ON schedules.prescription_id = prescriptions.id\r\n"
+            + "JOIN drug_users ON prescriptions.drug_user_id = drug_users.id\r\n"
+            + "WHERE prescriptions.is_active = 1\r\n"
+            + "  AND schedules.is_active = 1\r\n"
+            + "  AND drug_users.is_active = 1\r\n"
+            + "  AND NOT EXISTS (\r\n"
+            + "    SELECT 1\r\n"
+            + "    FROM confirm_notifications\r\n"
+            + "    WHERE confirm_notifications.schedule_id = schedules.id\r\n"
+            + "      AND confirm_notifications.date = DATE_SUB(CURDATE(), INTERVAL 1 DAY)\r\n"
+            + ")\r\n"
+            + "ORDER BY schedules.time;\r\n", nativeQuery = true)
+    List<Schedule> getListScheduletoDefaultConfirm();
 
     Iterable<Schedule> findAllByPrescriptionDrugUserUserIdAndIsActiveIsTrueOrderByTimeAsc(String userId);
 
